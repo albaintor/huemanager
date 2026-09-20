80c2df06d84f75e9f765ed465849bed184c3e1ab
albaintor <118518828+albaintor@users.noreply.github.com>
2026-09-20 05:54:53 -0700

    Start HomeKit only when iOS scene becomes active

--- ios/HueManagerHomeSync/Sources/HueManagerHomeSyncApp.swift
+++ ios/HueManagerHomeSync/Sources/HueManagerHomeSyncApp.swift
@@ -3,12 +3,18 @@ import SwiftUI
 @main
 struct HueManagerHomeSyncApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    @Environment(\.scenePhase) private var scenePhase
     @StateObject private var model = HomeSyncModel.shared
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(model)
+        }
+        .onChange(of: scenePhase) { _, newPhase in
+            if newPhase == .active {
+                model.start()
+            }
         }
     }
 }
